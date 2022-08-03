497eaae8fc8be911a65c6bf7784873642172ab2f
Scott Aron Bloom <scott@towel42.com>
2022-08-04 00:28:58 +0100

    Fix for typo

--- SystemInfo_win.cpp
+++ SystemInfo_win.cpp
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 #include "SystemInfo.h"
-#include "nodsicard.h"
+#include "nodiscard.h"
 
 #include <QDateTime>
 #include <QTimeZone>
